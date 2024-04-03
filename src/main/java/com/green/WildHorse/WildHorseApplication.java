1891576dc5fbb8b74e31a3de72469484620ab6fc
gui4458 <gui4458@gmail.com>
2024-04-03 11:46:24 +0900

    main

---
+++ src/main/java/com/green/WildHorse/WildHorseApplication.java
@@ -0,0 +1,13 @@
+package com.green.WildHorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildHorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildHorseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/WildHorse/WildHorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.WildHorse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildHorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
